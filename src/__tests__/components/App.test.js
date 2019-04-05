b4f8433e77cf6d91d0b2b701e4ea7a091e60ff72
Leonardo Alfaya Fonseca <leo.alfaya@gmail.com>
2019-04-05 15:01:29 -0300

    App component tested

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../../components/App';
 
-describe('App Component tests', () => {
-    test('renders without crashing', () => {
+describe('</App>', () => {
+    test('App should mount without errors', () => {
         const div = document.createElement('div')
-        ReactDOM.render(<App />, div)
-    })
+        ReactDOM.render(<App />, div);
+    });
+
+    test('App should render without errors', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toMatchSnapshot();
+    });
 })
